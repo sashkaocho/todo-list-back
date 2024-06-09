064f2f872e4e60b8e5af8daea9616775f00736ae
sashkaochigava <sashkaochi@gmail.com>
2024-06-10 01:28:17 +0400

    feat: added fillable property to work with post and patch requests

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'start_date',
+        'end_date'
+    ];
 }
